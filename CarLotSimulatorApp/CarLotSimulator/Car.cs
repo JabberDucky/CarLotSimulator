e9868180ce243ec2ceaf481b44e8550e2df9f0af
Katherine Vail <jabberduckygame@gmail.com>
2023-05-22 11:48:59 -0500

    completed exercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        public Car(int year, string make, string model, string engineNoise, string honkNoise, bool isDriveable )
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDrivable = isDriveable;
+        }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDrivable { get; set; }
+
+        public void MakeEngineNoise()
+        {
+            Console.WriteLine("Vroom.");
+        }
+
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine("Beep beep.");
+        }
+
+        public void MyCar()
+        {
+            Console.WriteLine($"My car is a {Year} {Model} {Make}. It is {IsDrivable} that it can drive and it likes to go:");
+        }
+
+        public void MyCar2()
+        {
+            Console.WriteLine($"My car is a {Year} {Model} {Make}. It is {IsDrivable} that it can drive and it likes to go:\n{EngineNoise}.\n{HonkNoise}.");
+        }
+    }
+
+    
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -28,6 +28,45 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            var carlot = new CarLot();
+
+            var car1 = new Car();
+            car1.Year = 2024;
+            car1.Make = "Tesla";
+            car1.Model = "Model S";
+            car1.IsDrivable = true;
+
+            car1.MyCar();
+            car1.MakeEngineNoise();
+            car1.MakeHonkNoise();
+
+            carlot.ListOfCars.Add(car1);
+
+            var car2 = new Car()
+            {
+                Year = 2023,
+                Make = "Subaru",
+                Model = "Outback",
+                IsDrivable = true,
+            };
+
+            car2.MyCar();
+            car2.MakeEngineNoise();
+            car2.MakeHonkNoise();
+
+            carlot.ListOfCars.Add(car2);
+
+            var car3 = new Car(2006, "R Corvette", "C6", "Vroom", "Beep beep.", true);
+
+            car3.MyCar2();
+
+            carlot.ListOfCars.Add(car3);
+
+            foreach(var car in carlot.ListOfCars)
+            {
+                Console.WriteLine($"This is a {car.Make} {car.Model} {car.Year}");
+            }
         }
     }
 }
